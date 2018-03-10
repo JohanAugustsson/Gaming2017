1e5494630cb3f0b9676d67ec2cc7d29443808742
Johan Augustsson <j.augustsson80@gmail.com>
2018-03-10 16:26:32 +0100

    remove seumantic

--- src/components/player-list/player-list.js
+++ src/components/player-list/player-list.js
@@ -1,31 +1,20 @@
 import React from "react";
-import Icon from "semantic-ui-react/dist/es/elements/Icon/Icon";
-import Table from "semantic-ui-react/dist/es/collections/Table/Table";
-import { Button } from 'semantic-ui-react'
+import './player-list.css'
 
 export const PlayerList = (props) => {
 
     if (props.players) {
         return (
             <div>
-                <Table celled inverted selectable >
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>All Players</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {Object.keys(props.players).map(selected =>
-                            <Table.Row key={props.players[selected].id}>
-                                <Table.Cell> <Icon name="target"/>
-                                {props.players[selected].name}
-                                <Button inverted size='small' compact floated='right' color='red' name={props.players[selected].name} onClick={props.removePlayer}>x</Button>
-                                </Table.Cell>
-
-                            </Table.Row>
-                        )}
-                    </Table.Body>
-                </Table>
+              All Players
+              <ul className="listOfPlayers">
+                  {Object.keys(props.players).map(selected =>
+                    <li key={props.players[selected].id}>
+                      {props.players[selected].name}
+                      <button name={props.players[selected].name} onClick={props.removePlayer}>x</button>
+                    </li>
+                  )}
+              </ul>
 
             </div>
         )
